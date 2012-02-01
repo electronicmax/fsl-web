2304b56040162a1506d5ad4ca7f7af082c62b82f
electronic Max <electronic@gmail.com>
2012-02-01 14:45:14 +0000

    scrolling issues resolved

--- js/fsl-main.js
+++ js/fsl-main.js
@@ -1,7 +1,26 @@
 require(
     [],    
     function() {
-	setTimeout(function() {
-		       $('.intro').removeClass('intro');
-		   },1000);
+	setTimeout(function() {  $('.intro').removeClass('intro');   },1000);
+
+        var parallaxScroll = function(){
+            var scrolled = $(window).scrollTop();
+	    $('#parallax-bg1').css('top',(0-(scrolled*.25))+'px');
+	    $('#parallax-bg2').css('top',(0-(scrolled*.5))+'px');
+	    $('#parallax-bg3').css('top',(0-(scrolled*.75))+'px'); 
+        };
+
+        $('nav .welcome').bind('click', function() {
+                                   console.log('click');
+                                   $('html, body').animate({ scrollTop:0 }, 1000, parallaxScroll);
+                                   return false;
+                               });
+        $('nav .webbox').bind('click', function() {
+                                   $('html, body').animate({scrollTop:$('#webbox').offset().top }, 1000, parallaxScroll);
+                                  return false;
+                               });
+        $('nav .about').bind('click', function() {
+                             $('html, body').animate({scrollTop:$('#about').offset().top }, 1000, parallaxScroll);
+                             return false;
+                         });        
     });
