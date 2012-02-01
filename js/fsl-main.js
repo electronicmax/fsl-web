408bdc0f02931481656dc4faa8b8dfcfce8b02c6
electronic Max <electronic@gmail.com>
2012-02-01 14:58:48 +0000

    got rid of construciton lines

--- js/fsl-main.js
+++ js/fsl-main.js
@@ -15,11 +15,11 @@ require(
                                    $('html, body').animate({ scrollTop:0 }, 1000, parallaxScroll);
                                    return false;
                                });
-        $('nav .webbox').bind('click', function() {
+        $('nav .webbox, #welcome div.next_arrow').bind('click', function() {
                                    $('html, body').animate({scrollTop:$('#webbox').offset().top }, 1000, parallaxScroll);
                                   return false;
                                });
-        $('nav .about').bind('click', function() {
+        $('nav .about, #webbox div.next_arrow').bind('click', function() {
                              $('html, body').animate({scrollTop:$('#about').offset().top }, 1000, parallaxScroll);
                              return false;
                          });        
