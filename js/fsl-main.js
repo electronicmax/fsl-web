a01b540a995ffc6fb7d172bcf117751627b7088d
electronic <electronic@ubuntu.(none)>
2012-02-07 19:53:36 +0000

    updated for fsl

--- js/fsl-main.js
+++ js/fsl-main.js
@@ -5,29 +5,29 @@ require(
 
         var parallaxScroll = function(){
             var scrolled = $(window).scrollTop();
-            $('#blrain').css('top',(420-(2*scrolled))+'px');
-            console.log((2400-(3*scrolled)));
-            $('#blbox').css('top',(4150-(3*scrolled))+'px');
+            $('#blrain').css('top',(350-(2*scrolled))+'px');
+            $('#blbox').css('top',(4000-(3*scrolled))+'px');
             
             //	    $('#blrain').css('top',(350-(scrolled*.25))+'px');
 	    // $('#blbox').css('top',(0-(scrolled*.5))+'px');
 	    // $('#parallax-bg3').css('top',(0-(scrolled*.75))+'px'); 
         };
 
-        $('nav .welcome').bind('click', function() {
-                                   console.log('click');
+        $('nav .welcome').bind('click',
+			       function() {
                                    $('html, body').animate({ scrollTop:0 }, 1000, parallaxScroll);
                                    return false;
                                });
-        $('nav .webbox, #welcome div.next_arrow').bind('click', function() {
-                                   $('html, body').animate({scrollTop:$('#webbox').offset().top }, 1000, parallaxScroll);
-                                  return false;
-                               });
-        $('nav .about, #webbox div.next_arrow').bind('click', function() {
-                             $('html, body').animate({scrollTop:$('#about').offset().top }, 1000, parallaxScroll);
-                             return false;
-                         });
-
+        $('nav .webbox, #welcome div.next_arrow').bind('click',
+						       function() {
+							   $('html, body').animate({scrollTop:$('#webbox').offset().top }, 1000, parallaxScroll);
+							   return false;
+						       });
+        $('nav .about, #webbox div.next_arrow').bind('click',
+						     function() {
+							 $('html, body').animate({scrollTop:$('#about').offset().top }, 1000, parallaxScroll);
+							 return false;
+						     });	
         $(window).bind('scroll',function(e){  parallaxScroll(); });
         parallaxScroll();            
     });
